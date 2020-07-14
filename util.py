d83bde24db4dd76d657d3056a20410864d8455f1
Omar Mohamed <42470033+omarmohamed101@users.noreply.github.com>
2020-07-14 04:21:03 +0200

    Create util.py

---
+++ util.py
@@ -0,0 +1,38 @@
+class Node():
+    def __init__(self, parent, movie, id):
+        self.parent = parent
+        self.movie = movie
+        self.id = id
+
+
+class StackFrontier():
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contains_state(self, state):
+        return any(node.state == state for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        if self.empty():
+            raise Exception("neighbors_for_person")
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
+
+
+class QueueFrontier(StackFrontier):
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
